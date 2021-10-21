3113e2a82364c206c589a2b665495edc6880cc0b
john horo <johnhoro125@gmail.com>
2021-10-21 23:33:28 +0530

    Manipulation

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,11 +1,11 @@
 // Select the h1 element and store it in a variable named heading.
-
+let heading = document.querySelector(`h1`);
 // Check the typeof heading and log it.
-
+console.log(typeof heading);
 // Change the color of heading to black.
-
+heading.style.backgroundColor = `black`;
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
-
+let allHrs = document.querySelectorAll(`hr`);
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
 
@@ -14,22 +14,42 @@ Array.from converts an array kind of data into array so we can use methods like 
 HINT:
 let allHrsArray = Array.from(allHrs)
 */
-
+let allHrsArray = Array.from(allHrs);
 // Set the border of the all the hr elements to "1px solid tomato"
+allHrsArray.forEach((ele) => {
+  ele.style.border = "1px solid tomato";
+});
 
 // Change the background of all the hr to "antiquewhite" using for of loop.
+allHrsArray.forEach((ele) => {
+  ele.style.backgroundColor = "antiquewhite";
+});
 
 // Change the 'border-radius' of all the hr to "5px" using array.
-
+allHrsArray.forEach((ele) => {
+  ele.style.borderRadius = "5px";
+});
 // Change the alignment of the heading(h1) to center.
+heading.style.textAlign = "center";
 
 // Change the font size of the heading to 3rem.
+heading.style.fontSize = "3rem";
 
 // Change the border of hr with class 'image' to `2px solid purple`.
+let hrImageArray = Array.from(hrImage);
+
+hrImageArray.forEach((ele) => {
+  ele.style.border = "2px solid purple";
+});
 
 // Hide the box number 17 (last box).
+let boxSeventeen = document.querySelector(".seventeen");
+boxSeventeen.style.display = "none";
 
 // Change the border of all the hr element from solid to dashed type
+allHrsArray.forEach((ele) => {
+  ele.style.border = "dashed";
+});
 
 // Create a pragraph element and store it in variable named 'para' using `createElement`
 
